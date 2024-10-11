a346c24fbd8b9a0d9730e88c55e340aff1f4168b
Alina <alinaa.tikhonova20@gmail.com>
2024-10-11 15:26:51 +0300

    first commit

---
+++ tasks/task1.js
@@ -0,0 +1,14 @@
+//Функція filterAdults приймає масив об'єктів, що представлє людей (з полями name та age),
+//вам потрібно написати код, який повертає новий масив, який містить лише тих людей, чий вік більше або дорівнює 18.
+function filterAdults(people) {
+    //Ваш код
+    let adults = [];
+  for (let i = 0; i < people.length; i++) {
+    if (people[i].age >= 18) {
+      adults.push(people[i]);
+    }
+  }
+  return adults;
+}
+  
+  module.exports = filterAdults;
